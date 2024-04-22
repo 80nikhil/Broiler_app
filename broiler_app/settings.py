247fbc994467f0c53f7619e1a7a9c091f95bd0e1
80nikhil <nikhilkarole808@gmail.com>
2024-04-22 14:35:50 +0530

    added database string

--- broiler_app/settings.py
+++ broiler_app/settings.py
@@ -82,17 +82,27 @@ WSGI_APPLICATION = 'broiler_app.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'broiler', 
+#         'USER': 'postgres',
+#         'PASSWORD': 'root',
+#         'HOST': '127.0.0.1', 
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'broiler', 
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1', 
+        'USER': 'nikhil',
+        'PASSWORD': 'broiler123',
+        'HOST': '172.31.32.148', 
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
