81cb9f64fea24e610c36b05f72708243b38a0e50
nikhiljava22 <160789223+nikhiljava22@users.noreply.github.com>
2024-04-26 21:27:17 +0530

    Update settings.py

--- broiler_app/settings.py
+++ broiler_app/settings.py
@@ -73,12 +73,12 @@ WSGI_APPLICATION = 'broiler_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
-}
+# DATABASES = {
+#      'default': {
+#          'ENGINE': 'django.db.backends.sqlite3',
+#          'NAME': BASE_DIR / 'db.sqlite3',
+#      }
+# }
 
 # DATABASES = {
 #   'default': {
@@ -91,16 +91,16 @@ DATABASES = {
 #    }
 #}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'broiler', 
-#         'USER': 'nikhil',
-#         'PASSWORD': 'broiler@123',
-#         'HOST': 'localhost',
-#         'PORT': '',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'broiler', 
+        'USER': 'broiler',
+        'PASSWORD': 'Broiler123',
+        'HOST': 'broiler.cpy2kcwsebks.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
