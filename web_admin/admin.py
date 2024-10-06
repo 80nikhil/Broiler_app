cd2b6581724529d1d6b36380879fc2ffd794a527
80nikhil <nikhilkarole808@gmail.com>
2024-10-06 20:04:13 +0530

    new c hanges

--- web_admin/admin.py
+++ web_admin/admin.py
@@ -4,13 +4,11 @@ from . models import *
 admin.site.register(user)
 class ctype_data_Admin(admin.ModelAdmin):
     model = ctype_data
+    search_fields = ['id','title','abbreviation','alias_name']
     list_display = ['id','title','abbreviation','alias_name']
-    def get_question(self, obj):
-        return obj.question.question
 admin.site.register(ctype_data,ctype_data_Admin)
 class broiler_region_Admin(admin.ModelAdmin):
     model = broiler_region
+    search_fields = ['id','regionid','region','abbreviation','alias_name']
     list_display = ['id','regionid','region','abbreviation','alias_name']
-    def get_question(self, obj):
-        return obj.question.question
 admin.site.register(broiler_region,broiler_region_Admin)
